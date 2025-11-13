187c4b1ef2213356b436a419a917f20de2038ad2
Theo Arini <tarini@z2r12p1.42lyon.fr>
2025-11-13 13:51:01 +0100

    finish

--- ex03/.objs/srcs/FragTrap.d
+++
@@ -1,6 +0,0 @@
-.objs/srcs/FragTrap.o: srcs/FragTrap.cpp srcs/../includes/FragTrap.hpp \
-  includes/ClapTrap.hpp
-
-srcs/../includes/FragTrap.hpp:
-
-includes/ClapTrap.hpp:
--- ex03/.objs/srcs/ScavTrap.d
+++
@@ -1,6 +0,0 @@
-.objs/srcs/ScavTrap.o: srcs/ScavTrap.cpp srcs/../includes/ScavTrap.hpp \
-  includes/ClapTrap.hpp
-
-srcs/../includes/ScavTrap.hpp:
-
-includes/ClapTrap.hpp:
